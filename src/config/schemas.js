3ff06db60890bdd2317ed6038898f893203bd4fe
xAartT <arturbresolin20@gmail.com>
2026-02-12 23:01:17 -0300

    fix: a

--- src/config/schemas.js
+++ src/config/schemas.js
@@ -1,17 +1,5 @@
 const sql = 
 `
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
 --
 -- TOC entry 224 (class 1259 OID 73903)
 -- Name: campos_setores; Type: TABLE; Schema: public; Owner: -
